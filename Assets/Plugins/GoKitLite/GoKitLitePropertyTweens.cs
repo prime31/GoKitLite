9d715d0f90882469f811f22328b2645ea0a9a4a4
Mike <mike@prime31.com>
2013-09-06 14:19:16 -0700

    fixed WinPhone/Win8 defines

--- Assets/Plugins/GoKitLite/GoKitLitePropertyTweens.cs
+++ Assets/Plugins/GoKitLite/GoKitLitePropertyTweens.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using System;
+using System.Reflection;
 
 
 namespace Prime31.GoKitLite
@@ -16,52 +17,52 @@ namespace Prime31.GoKitLite
 		/// </summary>
 		public static T setterForProperty<T>( System.Object targetObject, string propertyName )
 		{
-			// first get the property
-#if NET_CORE
-			var propInfo = targetObject.GetType().GetRuntimeProperty( propertyName );
+				// first get the property
+#if NETFX_CORE
+				var propInfo = targetObject.GetType().GetRuntimeProperty( propertyName );
 #else
-			var propInfo = targetObject.GetType().GetProperty( propertyName );
+				var propInfo = targetObject.GetType().GetProperty( propertyName );
 #endif
-			
-			if( propInfo == null )
-			{
-				Debug.Log( "could not find property with name: " + propertyName );
-				return default( T );
-			}
-			
-#if NET_CORE
-			// Windows Phone/Store new API
-			return (T)(object)propInfo.SetMethod.CreateDelegate( typeof( T ), targetObject );
+
+				if( propInfo == null )
+				{
+					Debug.Log( "could not find property with name: " + propertyName );
+					return default( T );
+				}
+
+#if NETFX_CORE
+				// Windows Phone/Store new API
+				return (T)(object)propInfo.SetMethod.CreateDelegate( typeof( T ), targetObject );
 #else
-			return (T)(object)Delegate.CreateDelegate( typeof( T ), targetObject, propInfo.GetSetMethod() );
+				return (T)(object)Delegate.CreateDelegate( typeof( T ), targetObject, propInfo.GetSetMethod() );
 #endif
 		}
-		
-		
+
+
 		/// <summary>
 		/// either returns a super fast Delegate to get the given property or null if it couldn't be found
 		/// via reflection
 		/// </summary>
 		public static T getterForProperty<T>( System.Object targetObject, string propertyName )
 		{
-			// first get the property
-#if NET_CORE
-			var propInfo = targetObject.GetType().GetRuntimeProperty( propertyName );
+				// first get the property
+#if NETFX_CORE
+				var propInfo = targetObject.GetType().GetRuntimeProperty( propertyName );
 #else
-			var propInfo = targetObject.GetType().GetProperty( propertyName );
+				var propInfo = targetObject.GetType().GetProperty( propertyName );
 #endif
-			
-			if( propInfo == null )
-			{
-				Debug.Log( "could not find property with name: " + propertyName );
-				return default( T );
-			}
-			
-#if NET_CORE
-			// Windows Phone/Store new API
-			return (T)(object)propInfo.GetMethod.CreateDelegate( typeof( T ), targetObject );
+
+				if( propInfo == null )
+				{
+					Debug.Log( "could not find property with name: " + propertyName );
+					return default( T );
+				}
+
+#if NETFX_CORE
+				// Windows Phone/Store new API
+				return (T)(object)propInfo.GetMethod.CreateDelegate( typeof( T ), targetObject );
 #else
-			return (T)(object)Delegate.CreateDelegate( typeof( T ), targetObject, propInfo.GetGetMethod() );
+				return (T)(object)Delegate.CreateDelegate( typeof( T ), targetObject, propInfo.GetGetMethod() );
 #endif
 		}
 	
